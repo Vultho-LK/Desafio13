9aa4c282624abbc6a556bd33f75ba39829796adc
Hemerson Luan <140278282+Vultho-LK@users.noreply.github.com>
2024-08-04 21:48:27 -0300

    Add files via upload

---
+++ Desafio13.py
@@ -0,0 +1,4 @@
+# calcular um aumento de 15% de um salario
+n1 = float(input('digite seu salario: '))
+aumento = n1 + 0.15 * n1
+print('seu salario de {:.2f} com 15% de aumento R${:.2f} com um aumento de R${:.2f}'.format(n1, aumento,0.15 * n1 ))
---
+++ README.md
@@ -0,0 +1,3 @@
+Calcula um aumento de 15% de um salario
+
+<img src=https://github.com/user-attachments/assets/1c54f799-6bd2-4942-9cd9-f47da9301488 width=100>
